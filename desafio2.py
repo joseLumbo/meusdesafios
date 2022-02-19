ce91a6120a7ee0b7ca2e2425a0223bc67de8d21f
Jose Lumbo <95136752+joseLumbo@users.noreply.github.com>
2022-02-19 20:13:43 -0300

    Update desafio2.py
    Código comentado

--- desafio2.py
+++ desafio2.py
@@ -1,54 +1,57 @@
-# Está funcionando:
-def validar_letra_maiuscula(senha):
-    count = 0
-    for index, character in enumerate(senha):
-        if character.upper() == senha[index] and senha[index] not in ('!@#$%^&()-+/123456789'):
+
+#Essa função validará a existencia de letra maiúscula
+def validar_maiuscula(senha):
+    count = 0 #Contará a letra
+    for index, character in enumerate(senha):#Itera sobre a senha
+        #Vai verificar se existe letra maiúscula
+        if character.upper() == senha[index] and senha[index] not in list('!@#$%^&()-+/123456789'):
             count += 1
     else:
+        #Caso contador menor de zero implica que não contem nehuma letra maiúscula o contrario indica que existe
         if count <= 0:
             print('Senha não contem letra maíuscula!')
         else:
-            return validar_letra_minuscula(senha)
+            return validar_minuscula(senha)
 
-# funcionando:
-def validar_letra_minuscula(senha):
-    conta_minuscula = 0
+#Essa função validará a existencia de letra mainuscula
+def validar_minuscula(senha):
+    conta_minuscula = 0 #Contará a letra
     for k in list(senha):
-        if k in ('qwertyuiopasdfghjklzxcvbnm'):
+        if k in list('qwertyuiopasdfghjklzxcvbnm'):
             conta_minuscula += 1
-
+   #Caso contador menor de zero implica que não contem nehuma letra minuscula o contrario indica que existe
     if conta_minuscula <= 0:
         print('Senha não contem letra minuscula!')
     else:
-        return validar_tamanho_senha(senha)
+        return validar_senha(senha)
 
 
-# funcionando:
-def validar_tamanho_senha(senha):
+#Esta função vai validar o tamanho
+def validar_tamanho(senha):
     if len(senha) < 6:
         print('Sua senha é inferior a seis caracteres!')
     else:
-        return validar_existencia_numero(senha)
+        return validar_numero(senha)
 
-# funcionando:
-def validar_existencia_numero(senha):
+# Validar a existencia de numeros
+def validar_numero(senha):
     contar_numero = 0
     for k in list(senha):
         if k in set('0123456789'):
             contar_numero += 1
     if contar_numero <= 0:
-        print('Senha não contem pelo menos um número!')
+        print('Senha não contem nenhum número!')
     else:
-        return validar_caracter_especial(senha)
+        return validar_especial(senha)
 
-
-def validar_caracter_especial(senha):
-    contar_caracter_especial = 0
+#Validadr a existencia de caracter especial
+def validar_especial(senha):
+    contar_especial = 0
     for caracter_especial in list(senha):
         if caracter_especial in ('!@#$%^&()-+/\'\\"'):
-            contar_caracter_especial += 1
-    if contar_caracter_especial <= 0:
-        print('Senha não contem pelo menos um caracter especial!')
+            contar_especial += 1
+    if contar_especial <= 0:
+        print('Senha não contem nenhum caracter especial!')
     else:
        
                 
@@ -56,4 +59,4 @@ def validar_caracter_especial(senha):
 
 
 password = input('Digite sua senha: ')
-validar_letra_maiuscula(password)
+validar_maiuscula(password)
