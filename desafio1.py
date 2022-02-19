b8575714adf786194dfb4345615a6627646051d7
Jose Lumbo <95136752+joseLumbo@users.noreply.github.com>
2022-02-19 19:42:33 -0300

    Update desafio1.py
    Adição de comentário sobre o codigo

--- desafio1.py
+++ desafio1.py
@@ -1,9 +1,14 @@
-def escada_1(n):
+
+#Criando função para imprimir o número de degraus
+def escada(n):
+    #Iterando sobre o numero de degraus
     for k in range(1, n + 1):
         for i in range(n, k, -1):
+            #Imprimindo o espassamento
             print("", end = " ")
         for z in range(0, k):
+            #Imprimindo o asteristico em função a quantidade no range
             print("*", end = '')
         print()
 
-escada_1(10)
+escada(10)
