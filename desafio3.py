90e69614415729d7b8e4d81d3b95f9e9b881087d
Jose Lumbo <95136752+joseLumbo@users.noreply.github.com>
2022-02-19 20:24:06 -0300

    Update desafio3.py
    Códigos comentado

--- desafio3.py
+++ desafio3.py
@@ -1,15 +1,17 @@
-def main(palavra):
+
+#Esta função encontra os numeros de anagramas na posição para
+def main(word):
     contar = 0
-    for i in range(0, len(palavra)-1):
-        for k in range (1, len(palavra)-i):
-            slices1 = palavra[i:i+k]
-            for j in range (i+1 , len(palavra)):
-                slices2 = palavra[j:j+k]
-                if sorted(list(slices2)) == sorted(list(slices1)):
+    for i in range(0, len(word)-1):
+        for k in range (1, len(word)-i):
+            word_part1 = word[i:i+k]
+            for j in range (i+1 , len(word)):
+                word_part2 = word[j:j+k]
+                if sorted(list(word_part2)) == sorted(list(word_part1)):
                     contar += 1
-                    print(f'({slices1}, {slices2})')
+                    print(f'({word_part1}, {word_part2})')
 
     print(contar)
 
-
-main(' palavra ')
+#Chamando a função main
+main(' word ')
